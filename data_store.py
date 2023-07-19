da778668e83fc08b6ebef8b9c1f2d9cba01843a4
Mike-Novoselov <68411205+Mike-Novoselov@users.noreply.github.com>
2023-07-19 15:39:29 +0700

    Update data_store.py

--- data_store.py
+++ data_store.py
@@ -10,12 +10,14 @@ from config import db_url_object
 metadata = MetaData()
 Base = declarative_base()
 
+engine = create_engine(db_url_object)
+
 
 class Viewed(Base):
     __tablename__ = 'viewed'
     profile_id = sq.Column(sq.Integer, primary_key=True)
     worksheet_id = sq.Column(sq.Integer, primary_key=True)
-
+    
 
 # добавление записи в бд
 
@@ -29,6 +31,7 @@ def add_user(engine, profile_id, worksheet_id):
 
 # извлечение записей из БД
 
+
 def check_user(engine, profile_id, worksheet_id):
     with Session(engine) as session:
         from_bd = session.query(Viewed).filter(
@@ -36,10 +39,9 @@ def check_user(engine, profile_id, worksheet_id):
             Viewed.worksheet_id == worksheet_id
         ).first()
         return True if from_bd else False
-
+    
 
 if __name__ == '__main__':
-    engine = create_engine(db_url_object)
     Base.metadata.create_all(engine)
     # add_user(engine, 2113, 124512)
     res = check_user(engine, 2113, 1245121)
